d8b8e1c5fb5e39d92cb6d2827d03982530e90c13
Sarah <sarah.evans@qq.com>
2019-02-24 05:21:17 -0600

    Added logging to role command

--- commands/role.js
+++ commands/role.js
@@ -1,38 +1,47 @@
 exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
 
-  var args1 = args.join(" ");
-  args1 = args1.replace("@", "");
+  var inRole = args.join(" ");
+  inRole = inRole.replace("@", "");
   const allRoles = client.getAllAssignableRoles();
+
   var readableRoles = allRoles.forEach(function(roleName) {
     readableRoles +  `**${roleName}** `;
   });
+
+  // This role is not assignable, as it is not in the assignable role list.
   if (getRoleName(args1) == "NULL")
     return message.channel.send("Please specify a valid role: " + readableRoles + " !role [name]");
 
+  // No role was specified
   if (args == "")
     return message.channel.send("Please specify a valid role: " + readableRoles + " !role [name]");
 
-  const role = message.guild.roles.find("name", getRoleName(args1));
-  args1 = getRoleName(args1);
+  // Attempt to find the role on the server.
+  const role = message.guild.roles.find("name", getRoleName(inRole));
 
+  inRole = getRoleName(inRole);
+
+  // This role was not found on the server.
   if (role == null)
-    return message.channel.send(`Are you sure ` + "`" + args1 + "`" + ` is a valid role?`);
+    return message.channel.send(`Are you sure ` + "`" + inRole + "`" + ` is a valid role?`);
 
-  if (!client.config.defaultSettings.enabledRoles.includes(args1))
+  // This role is not self assignable.
+  if (!client.config.defaultSettings.enabledRoles.includes(inRole))
       return message.channel.send("`" + role.name + "` is not self-assignable. Available roles: " + readableRoles);
 
+  // This role is unassignable, due to Discord role hierarchy.
   if (message.channel.guild.me.highestRole.comparePositionTo(role) <= 0)
     return message.channel.send("`" + role.name + "` is above my highest role. I cannot set that role.");
 
+  // The correct permission is not assigned to this bot.
   if (!message.channel.permissionsFor(message.guild.me).hasPermission("MANAGE_ROLES_OR_PERMISSIONS"))
-    return message.channel.send("I don't have the correct permissions for that!");
+    return message.channel.send("I don't have the correct permissions for that! I need `MANAGE_ROLES_OR_PERMISSIONS`");
 
+  // This role is assignable, they do not have the role, so let's give it to them!
   if (!message.member.roles.has(role.id)) {
     message.member.addRole(role).catch(console.error);
     message.channel.send("You now have the `" + role.name + "` role.");
-  }
-
-  else {
+  } else { // This role is assignable, they do have the role, so let's remove it from them!
     message.member.removeRole(role).catch(console.error);
     message.channel.send("You no longer have the `" + role.name + "` role.");
   }
@@ -82,7 +91,7 @@ exports.conf = {
 
 exports.help = {
   name: "role",
-  category: "Miscellaneous",
+  category: "Renegade X",
   description: "Give yourself a role.",
   usage: "role [role]"
 };
